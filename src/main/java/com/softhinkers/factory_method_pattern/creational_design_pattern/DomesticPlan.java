7d03c559d5b7e28db8be85e768cbca1f13aa0912
Adarsh Maurya <adarshmauryatalks@gmail.com>
2020-04-26 14:51:03 +0530

    - moved files to new package name

--- src/main/java/com/softhinkers/factory_method_pattern/creational_design_pattern/DomesticPlan.java
+++
@@ -1,8 +0,0 @@
-package com.softhinkers.factory_method_pattern.creational_design_pattern;
-
-public class DomesticPlan extends Plan {
-
-    void getRate() {
-        rate = 3.50;
-    }
-}
--- src/main/java/com/softhinkers/factory_method_pattern/creational_design_pattern/InstitutionalPlan.java
+++
@@ -1,7 +0,0 @@
-package com.softhinkers.factory_method_pattern.creational_design_pattern;
-
-public class InstitutionalPlan extends Plan {
-    void getRate() {
-        rate = 5.50;
-    }
-}
